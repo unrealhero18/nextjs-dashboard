46cfa8b31d8afaec75ead460597cc15f257f57c5
Kostiantyn Filimonov <unrealhero18@gmail.com>
2026-04-02 11:48:56 +0300

    feat: implement authentication, invoice CRUD actions, and server-side data fetching

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,37 @@
+import { notFound } from 'next/navigation';
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+type PageProps = {
+  params: Promise<{ id: string }>
+}
+
+export default async function Page(props: PageProps) {
+  const params = await props.params;
+  const { id } = params;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
---
+++ proxy.ts
@@ -0,0 +1,9 @@
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export const { auth: proxy } = NextAuth(authConfig);
+
+export const config = {
+  // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
